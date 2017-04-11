62887d2c4a0f7df021601db1bbe4f001c13dbedd
Scott Stanfield <scottstanfield@gmail.com>
2017-04-10 18:40:43 -0700

    Important compiler flags for R libraries
    Without the CXX1* flags, I couldn't get XGBoost nor LightGBM to build.
    It took about *4 hours* to track down this issue.
    
    I don't know what the optimal parameters should be here, but this is working.
    
    Details at
    https://stackoverflow.com/questions/23414448/r-makevars-file-to-overwrite-r-cmds-default-g-options

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ mkdir -p ~/.config/nvim
 link config/nvim/init.vim ~/.config/nvim/init.vim
 
 # Setup R
-mkdir -p ~/R/library
+mkdir -p ~/.R/library
 link R/Makevars ~/.R/Makevars
 link Renviron ~/.Renviron
 link Rprofile ~/.Rprofile
