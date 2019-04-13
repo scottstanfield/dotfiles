b6af542828de3fbe2061c5f5032f95ebe30f8720
Scott Stanfield <scott@stanfield.io>
2019-04-13 11:40:13 -0700

    Added status messages

--- R/install.r.packages.r
+++ R/install.r.packages.r
@@ -4,13 +4,27 @@ r <- getOption('repos')
 r["CRAN"] <- "http://cran.cnr.berkeley.edu"
 options(repos=r)
 
-# install.packages('data.table', repos='https://Rdatatable.github.io/data.table')
+cat("§ data.table \n")
+install.packages('data.table', repos='https://Rdatatable.github.io/data.table')
+
+# for %>% pipe
+cat("§ magrittr \n")
 install.packages('magrittr')
 
-# install.packages('remotes')		# a lighter-weight devtools package
-# remotes::install_github('jalvesaq/colorout')
-# remotes::install_github('cran/setwidth')
+# so we can install straight from github
+cat("§ remotes \n")
+install.packages('remotes')
 
-# install.packages('R.utils')   # for data.table gzip support
+# color in all console output
+cat("§ colorout \n")
+remotes::install_github('jalvesaq/colorout')
+
+# adapt R console output to terminal width
+cat("§ setwidth \n")
+remotes::install_github('cran/setwidth')
+
+# for data.table gzip support
+cat("§ R.utils \n")
+install.packages('R.utils')
 
 
