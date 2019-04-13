fb3deb4fcab3ac6cd95230c03e3506428d45ed9f
Scott Stanfield <scott@stanfield.io>
2019-04-13 11:20:49 -0700

    Need to manually set repo if running vanilla

--- R/install.r.packages.r
+++ R/install.r.packages.r
@@ -1,11 +1,16 @@
 #!Rscript --vanilla
 
-install.packages('data.table', repos='https://Rdatatable.github.io/data.table')
+r <- getOption('repos')
+r["CRAN"] <- "http://cran.cnr.berkeley.edu"
+options(repos=r)
+
+# install.packages('data.table', repos='https://Rdatatable.github.io/data.table')
 install.packages('magrittr')
 
-install.packages('remotes')		# a lighter-weight devtools package
-remotes::install_github('jalvesaq/colorout')
-remotes::install_github('cran/setwidth')
+# install.packages('remotes')		# a lighter-weight devtools package
+# remotes::install_github('jalvesaq/colorout')
+# remotes::install_github('cran/setwidth')
 
-install.packages('R.utils')   # for data.table gzip support
+# install.packages('R.utils')   # for data.table gzip support
+
 
