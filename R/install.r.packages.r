380ddb080c96db4ad38b74b6ff1da0bb4f0fd8eb
Scott Stanfield <scott@stanfield.io>
2020-02-16 21:16:31 -0800

    Streamline package install

--- R/install.r.packages.r
+++ R/install.r.packages.r
@@ -1,36 +1,9 @@
 #!/usr/bin/env Rscript
 
-cat("§ data.table \n")
-#install.packages('data.table', repos='https://Rdatatable.github.io/data.table')
 install.packages('data.table')
-
-# for %>% pipe
-cat("§ magrittr \n")
 install.packages('magrittr')
-
-# so we can install straight from github
-cat("§ remotes \n")
 install.packages('remotes')
-
-# color in all console output
-cat("§ colorout \n")
 remotes::install_github('jalvesaq/colorout')
-
-# Can't get these two packages to work consistently, so comment out for now
-
-# cd /tmp
-# git clone https://github.com/jalvesaq/colorout
-# R CMD INSTALL colorout
-
-# git clone https://github.com/cran/setwidth
-# R CMD INSTALL setwidth
-
-# adapt R console output to terminal width
-cat("§ setwidth \n")
 remotes::install_github('cran/setwidth')
-
-# for data.table gzip support
-cat("§ R.utils \n")
 install.packages('R.utils')
-
 
