14ad75e17314dd2c36bc04a43236aaa85b480501
Scott Stanfield <scott.stanfield@microsoft.com>
2021-02-14 14:37:08 -0800

    Add rust installer

--- os/raspbian.sh
+++ os/raspbian.sh
@@ -7,6 +7,5 @@ die()     { local ret=$?; printf "%s\n" "$@" >&2; exit "$ret"; }
 # https://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself
 readonly _D="$(dirname "$(readlink -f "$0")")" && cd "$_D"
 
-[[ $EUID -eq 0 ]] || die "$0 needs to run as root. Try sudo $0"
-
 sudo apt-install -y neovim zsh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
