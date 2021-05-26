acd922cb472a790e1680a2de9eed8e242d0ddb68
Scott Stanfield <scottstanfield@gmail.com>
2021-05-26 16:35:57 -0700

    Comment out the best part since no nerd font on Windows

--- p10k.zsh
+++ p10k.zsh
@@ -30,7 +30,7 @@
 
   # The list of segments shown on the left. Fill it with the most important segments.
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
-    os_icon               # os identifier
+    # os_icon               # os identifier
     dir                     # current directory
     prompt_char             # prompt symbol
   )
