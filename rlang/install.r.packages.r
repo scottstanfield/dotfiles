186e38ef4fc92b03e7758cd6e11c6dfc377fa81f
Scott Stanfield <scottstanfield@gmail.com>
2024-09-02 21:10:54 -0700

    Testing fastverse install in R

--- rlang/install.r.packages.r
+++ rlang/install.r.packages.r
@@ -7,3 +7,6 @@ remotes::install_github('jalvesaq/colorout')
 remotes::install_github('cran/setwidth')
 install.packages('R.utils')
 
+install.packages('tidyverse')
+remotes::install_github('2005m/kit')
+remotes::install_github('fastverse/fastverse')
