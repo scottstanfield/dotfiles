471d6fa59fa588ea6d7980c5e620e99bd8ae9565
Git Config dot Local <someone@example.com>
2025-02-18 21:13:05 -0800

    testing openmp library for R on macOS

--- rlang/openmp/hw.c
+++ rlang/openmp/hw.c
@@ -1,11 +1,13 @@
 // verifying that OMP environment actually works
 
-#include <stdio.h>
 #include <omp.h>
+#include <stdio.h>
 
 int main() {
-    #pragma omp parallel 
-    {
-        printf("Hello from %d\n", omp_get_thread_num());
-    }
+#pragma omp parallel
+  {
+    int x = 0;
+    printf("%d\n", x + 1);
+    printf("Hello from %d\n", omp_get_thread_num());
+  }
 }
---
+++ rlang/openmp/readme.md
@@ -0,0 +1,9 @@
+# header
+
+## second level
+
+This should be **bold** 
+
+- list second
+- so much pop up windows
+{()}
