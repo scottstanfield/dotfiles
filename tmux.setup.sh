b43989cc5dbf353409924af3b92e8a2557b2e828
Scott Stanfield <scottstanfield@gmail.com>
2025-08-21 21:30:28 -0700

    quicker clone

--- tmux.setup.sh
+++ tmux.setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone --depth=1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 echo "Now, launch tmux then run these two commands"
 echo "tmux source ~/.tmux.conf"
