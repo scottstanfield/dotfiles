6321d6735eb13b204e8fe6af6e4ddb393fbb388d
Scott Stanfield <scottstanfield@gmail.com>
2025-12-01 11:48:48 -0800

    Tests for missing XDG variables

--- neovim.plugins.sh
+++ neovim.plugins.sh
@@ -16,6 +16,8 @@ XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.config}"
 mkdir -p $XDG_DATA_HOME
 mkdir -p $XDG_CACHE_HOME
 
+xdg_data_home="${XDG_DATA_HOME:-$HOME/.local/share}"
+
 require nvim
 require git 
 require curl
@@ -25,8 +27,8 @@ else
     require gcc
 fi
 
-rm -rf $XDG_DATA_HOME/nvim/site
-curl -fLo $XDG_DATA_HOME/nvim/site/autoload/plug.vim --create-dirs \
+rm -rf $xdg_data_home/nvim/site
+curl -fLo $xdg_data_home/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 nvim --headless +':PlugInstall --sync' +qa
