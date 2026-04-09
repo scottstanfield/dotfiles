f04fa7d628e7f7def6eddf25f2642012a906d1b3
Scott Stanfield <scottstanfield@gmail.com>
2026-04-08 23:21:40 -0700

    Install build-essentials for debian

--- os/ephemeral-lima.sh
+++ os/ephemeral-lima.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 trap 'limactl stop deb; limactl delete --force deb' EXIT INT TERM
 
+echo "Run `lima shell deb` to connect"
 limactl start --foreground --name=deb lima-debian-trixie.yaml &
 wait
