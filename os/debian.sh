49a25f04137e66a6d015aeab9c18c086b61a1cf5
Scott Stanfield <scottstanfield@gmail.com>
2026-04-18 13:02:06 -0700

    Big batch

--- os/debian.sh
+++ os/debian.sh
@@ -4,7 +4,7 @@ set -Eeuo pipefail
 # https://unix.stackexchange.com/questions/65803/why-is-printf-better-than-echo
 println() { local IFS=" "; printf '%s\n' "$*"; }
 require() { hash "$@" 2>&- || exit 127; }
-die()     { local ret=$?; printf "%s\n" "$@" >&2; exit "$ret"; }
+die()     { local ret=$?; printf "%s\n" "$@" >&2; exit "${ret:-1}"; }
 
 readonly _D="$(dirname "$(readlink -f "$0")")"
 cd "$_D"
--- os/mise.sh
+++ os/mise.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
+
+# this is meant to be sourced like this:
+# . mise.sh
+#
 set -Eeuo pipefail
 
 # https://unix.stackexchange.com/questions/65803/why-is-printf-better-than-echo
 println() { local IFS=" "; printf '%s\n' "$*"; }
 require() { hash "$@" 2>&- || exit 127; }
-die()     { local ret=$?; printf "%s\n" "$@" >&2; exit "$ret"; }
+die()     { local ret=$?; printf "%s\n" "$@" >&2; exit "${ret:-1}"; }
 
 readonly _D="$(dirname "$(readlink -f "$0")")"
 cd "$_D"
@@ -17,7 +21,6 @@ fi
 
 if ! grep -q 'mise/shims' "$HOME/.bashrc"; then
     cat >> $HOME/.bashrc <<EOF
-
 # mise
 export PATH="$HOME/.local/bin:$HOME/.local/share/mise/shims:$PATH"
 EOF
